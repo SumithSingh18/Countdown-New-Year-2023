3ab7e6cf1d74dd2fc51cb92b2cc5fc79887f160f
Sumith Singh <72908644+SumithSingh18@users.noreply.github.com>
2022-01-22 16:41:52 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,33 @@
+const newYears = '1 Jan 2023';
+
+
+function countdown() {
+
+    const newYearDate = new Date(newYears);
+    const currentDate = new Date();
+
+    // let totalMonth = currentDate.getMonth();
+    
+    // const months = 11 - totalMonth;
+    const totalSeconds = (newYearDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    // document.getElementById('month').innerHTML = formatTime(months)
+    document.getElementById('days').innerHTML = formatTime(days);
+    document.getElementById('hours').innerHTML = formatTime(hours);
+    document.getElementById('minutes').innerHTML = formatTime(minutes)
+    document.getElementById('seconds').innerHTML = formatTime(seconds)
+
+}
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+
+//initial call
+countdown();
+
+setInterval(countdown, 1000);
